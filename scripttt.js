54d525709f3313db2f4760f8cff0866fbc27beb2
agungwowor <jongsaja0@gmail.com>
2024-08-04 21:00:55 +0900

    Update scripttt.js

--- scripttt.js
+++ scripttt.js
@@ -115,7 +115,7 @@ document.getElementById("kadoIn").onclick = function() {if(fungsiAwal==0){audio.
   kalimatc.innerHTML = "";kalimatc.style.display="none";
   opsL.style.opacity="0";
   new TypeIt("#kalimatd", {
-  strings: ["" + vketik4, "" + vketik5], startDelay: 100000, speed: 40, deleteSpeed: 10, breakLines: false, waitUntilVisible: true, lifelike: true,
+  strings: ["" + vketik4, "" + vketik5], startDelay: 100, Delay: 100000, speed: 40, deleteSpeed: 10, breakLines: false, waitUntilVisible: true, lifelike: true,
   afterComplete: function(){
     opsL.style.opacity=".8";opsLcheck=4;opsLclick=1;
   },}).go();
@@ -126,7 +126,7 @@ document.getElementById("kadoIn").onclick = function() {if(fungsiAwal==0){audio.
   kalimatd.innerHTML = "";kalimatd.style.display="none";
   opsL.style.opacity="0";
   new TypeIt("#kalimatf", {
-  strings: ["" + vketik6], startDelay: 100000, speed: 40, deleteSpeed: 10, breakLines: false, waitUntilVisible: true, lifelike: true,
+  strings: ["" + vketik6], startDelay: 100, speed: 40, deleteSpeed: 10, breakLines: false, waitUntilVisible: true, lifelike: true,
   afterComplete: function(){
      kalimatf.innerHTML = vketik6;
     opsLcheck=5;opsLclick=1;
